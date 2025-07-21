f288757d2604f80b3f8b13802543c586661e63f0
Yang jun mo <47310648+hjhjhj0917@users.noreply.github.com>
2025-07-21 17:56:49 +0900

    junmo (#1)

---
+++ src/main/java/kopo/poly/hanium/controller/QuizController.java
@@ -0,0 +1,19 @@
+package kopo.poly.hanium.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@RequestMapping(value = "/quiz")
+@RequiredArgsConstructor
+@Controller
+public class QuizController {
+
+    @GetMapping(value = "quizPage")
+    public String quizPage() {
+        return "quiz/quiz";
+    }
+}
