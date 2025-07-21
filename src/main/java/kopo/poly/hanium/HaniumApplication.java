02cf6b3175059cbac75957582693c54ae9bc8a7e
kimyega <kimyega@naver.com>
2025-07-21 14:03:47 +0900

    Initial commit

---
+++ src/main/java/kopo/poly/hanium/HaniumApplication.java
@@ -0,0 +1,13 @@
+package kopo.poly.hanium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaniumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaniumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kopo/poly/hanium/HaniumApplicationTests.java
@@ -0,0 +1,13 @@
+package kopo.poly.hanium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaniumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
