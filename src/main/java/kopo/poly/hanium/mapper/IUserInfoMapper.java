87398721408b16fab64f79cb36550862fa26b7ca
choijunyoung <yj766352@gmail.com>
2025-08-14 19:58:54 +0900

    모달창 구현

--- src/main/java/kopo/poly/hanium/mapper/IUserInfoMapper.java
+++ src/main/java/kopo/poly/hanium/mapper/IUserInfoMapper.java
@@ -11,6 +11,12 @@ public interface IUserInfoMapper {
     UserInfoDTO emailAuthNumber(UserInfoDTO pDTO) throws Exception;
 
     UserInfoDTO getUserId(UserInfoDTO pDTO) throws Exception;
+    UserInfoDTO getUserForPassword(UserInfoDTO pDTO) throws Exception;  // 비번 찾기 (user_id+email)
+
+    int updateUserPassword(UserInfoDTO pDTO) throws Exception;          // 비번 변경
+
+    int deleteUser(UserInfoDTO pDTO) throws Exception;                  // 회원 탈퇴
+
 
     // 회원가입
     int insertUserInfo(UserInfoDTO pDTO) throws Exception;
